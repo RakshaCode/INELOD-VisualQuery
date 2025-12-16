3ee3dc09276ded3e6e993ea6a4ee7db01faf6384
RakshaCode <71290241+RakshaCode@users.noreply.github.com>
2025-12-16 08:13:27 +0100

    Visualización de consulta

--- vocab/js/visual.js
+++ vocab/js/visual.js
@@ -10,9 +10,26 @@ $(document).ready(function () {
 
 // Ejecutar consulta SPARQL y devuelve los datos
 function executeSparqlQuery(query) {
-	return fetch(SPARQL_ENDPOINT + "?query=" + encodeURIComponent(query), {
+	fetch(SPARQL_ENDPOINT + "?query=" + encodeURIComponent(query), {
 		headers: { "Accept": "application/sparql-results+json" }
 	})
 		.then(response => response.json())
-		.then(data => data.results.bindings);
+		.then(data => {
+			// Encabezados
+			let headHtml = '<tr>';
+			data.head.vars.forEach(v => headHtml += '<th>' + v + '</th>');
+			headHtml += '</tr>';
+			document.getElementById('table-head').innerHTML = headHtml;
+
+			// Filas
+			let bodyHtml = '';
+			data.results.bindings.forEach(row => {
+				bodyHtml += '<tr>';
+				data.head.vars.forEach(v => {
+					bodyHtml += '<td>' + (row[v] ? row[v].value : '') + '</td>';
+				});
+				bodyHtml += '</tr>';
+			});
+			document.getElementById('table-body').innerHTML = bodyHtml;
+		});
 }
